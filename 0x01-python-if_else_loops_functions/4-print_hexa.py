4bf2deae987bcd86d92eac03250504eacf692663
michaelEmeka <mikelonu15@gmail.com>
2022-09-21 11:14:37 +0100

    Modify file 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
+
+#prints numbers 0-98 in hexadecimal
 for n in range(0, 99):
-	print("{} = 0x".format(n), end="")
-	if n < 10:
-		print(n)
-	elif n < 16:
-		print(chr(97 + n % 10))
-	else:
-		print(n // 16, end="")
-		if n % 16 > 9:
-			print(chr((97 + (n % 16 % 10))))
-		else:
-			print(n % 16)
+	print("{} = {}".format(n, hex(n))
