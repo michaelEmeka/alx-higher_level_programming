aec41735c3aff5147374cb90f29355f4aef8eaf2
michaelEmeka <mikelonu15@gmail.com>
2022-09-21 13:34:38 +0100

    Modify 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num % 3 == 0 and num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz ", end="")
     elif num % 3 == 0:
         print("Fizz ", end="")
