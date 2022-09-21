9f4aff241c65f370e29a757e19fdc4929844008b
michaelEmeka <mikelonu15@gmail.com>
2022-09-21 11:06:23 +0100

    Add 4-print_hexa.py

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for n in range(0, 99):
+	print("{} = 0x".format(n), end="")
+	if n < 10:
+		print(n)
+	elif n < 16:
+		print(chr(97 + n % 10))
+	else:
+		print("{}".format(n // 16), end="")
+		if n % 16 > 9:
+			print(chr((97 + (n % 16 % 10))))
+		else:
+			print(n % 16)
