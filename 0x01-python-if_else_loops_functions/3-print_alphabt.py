106f428c6f1d6a79df0290c233ef50b9cadfd57b
michaelEmeka <mikelonu15@gmail.com>
2022-09-21 07:53:12 +0100

    Modify file 2 and 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for letter in range(97, 123):
     if chr(letter) == 'q' or chr(letter) == 'e':
         continue
     else:
-        print("{}".format(chr(letter)), end = "")
+        print("{}".format(chr(letter)), end="")
