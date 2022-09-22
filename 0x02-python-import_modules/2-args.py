51857b102c49b8af9d2022e52d5242ce825f1067
michaelEmeka <mikelonu15@gmail.com>
2022-09-23 00:49:52 +0100

    Add 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(argc - 1))
-        for i in range(argc):
-            print("{}: {}".format((i + 1), argv[i + 1]))
+        for i in range(1, argc):
+            print("{}: {}".format((i), argv[i]))
