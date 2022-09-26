fcd7ea0a31cf85f6e14ebbb4decaca1dd7791e66
michaelEmeka <mikelonu15@gmail.com>
2022-09-26 19:15:13 +0100

    Add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    height = len(matrix)
+    width = len(matrix[0])
+    for row in range(height):
+        for col in range(width):
+            print("{:d}{}".format(matrix[row][col], " " if col + 1 != width) else "", end="")
+        print()
