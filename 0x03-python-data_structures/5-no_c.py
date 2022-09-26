0be92fd451fa84e73d62260e402dc34ee1e06473
michaelEmeka <mikelonu15@gmail.com>
2022-09-26 18:32:32 +0100

    Modify file 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     my_string = list(my_string)
     delim = "cC"
     for i in range(length):
-        if length[i] in delim:
+        if my_string[i] in delim:
             my_string.remove(my_string[i])
     my_string = "".join(my_string)
     return my_string
