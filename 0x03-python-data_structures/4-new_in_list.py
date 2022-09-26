2c7b6188d1832eda14bd065207a1379a588b9040
michaelEmeka <mikelonu15@gmail.com>
2022-09-26 16:42:11 +0100

    Add 4+new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    clone_list = my_list
+    length = len(my_list)
+    if idx < 0:
+        return clone_list
+    if idx >= length:
+        return clone_list
+    for i in range(length):
+        if i == idx:
+            clone_list[i] = element
+            return clone_list
