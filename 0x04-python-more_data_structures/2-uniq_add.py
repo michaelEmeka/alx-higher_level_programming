52f3e4d8084e304af79ce1535db82a5fa0199a46
michaelEmeka <mikelonu15@gmail.com>
2022-09-29 22:50:18 +0100

    Modify file 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     calc = 0
+    passed = []
     for n in my_list:
-        if my_list.count(n) == 1:
+        if n not in passed:
             calc += n
+        passed.append(n)
     return calc
