3cdb84d8c97b66cbdf4bb1f89dea96d852ad64ec
michaelEmeka <mikelonu15@gmail.com>
2022-09-29 20:23:02 +0100

    Modify file 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     sqr_matrix = matrix.copy()
-    for i in matrix:
+    for i in range(len(matrix)):
         sqr_matrix[i] = [x**2 for x in matrix[i]]
     return sqr_matrix
