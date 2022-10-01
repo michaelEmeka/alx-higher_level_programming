5d2e121bb3b657ade6f2e8e8967e55dc83f41e95
michaelEmeka <mikelonu15@gmail.com>
2022-10-01 11:04:33 +0100

    Modify 7 and 8

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    new_dict.update({key: value})
-    return new_dict
+    a_dictionary.update({key: value})
+    return a_dictionary
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = a_dictionary.copy()
-    new_dict.pop(key)
-    return new_dict
+    a_dictionary.pop(key)
+    return a_dictionary
