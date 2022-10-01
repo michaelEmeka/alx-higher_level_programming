3e7e6591331da4c840921fe4ea10f113767de330
michaelEmeka <mikelonu15@gmail.com>
2022-10-01 15:20:09 +0100

    Modify file 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-a_dict = a_dictionary
-if a_dict == None:
-	return None
-keyz = list(a_dict.keys())
-maxK = keyz[0]
-for key in keyz:
-	if a_dict.get(key) > a_dict.get(maxK):
-		maxK = key
-return maxK
+def best_score(a_dictionary):
+    a_dict = a_dictionary
+
+    if a_dict == None:
+        return None
+
+    keyz = list(a_dict.keys())
+    maxK = keyz[0]
+
+    for key in keyz:
+        if a_dict.get(key) > a_dict.get(maxK):
+            maxK = key
+
+    return maxK
