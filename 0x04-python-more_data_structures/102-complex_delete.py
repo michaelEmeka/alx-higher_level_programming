0e4fbe87d65d263e302aa4a8d59b33469ca0160a
michaelEmeka <mikelonu15@gmail.com>
2022-10-02 00:25:28 +0100

    Modify file 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     a_dict = a_dictionary
-    for key in a_dict:
+    for key in list(a_dict):
         if a_dict.get(key) == value:
             a_dict.pop(key)
