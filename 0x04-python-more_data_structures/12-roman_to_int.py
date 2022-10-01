4c0b1101064d9e5e6bc4efaf37cf99f355be868a
michaelEmeka <mikelonu15@gmail.com>
2022-10-01 16:00:55 +0100

    Modify file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-	rome = {'M' : 1000, 'D': 500, 'C': 100, 'L': 50, 'X':10, 'V': 5, 'I':1}
+    rome = {'M' : 1000, 'D': 500, 'C': 100, 'L': 50, 'X':10, 'V': 5, 'I':1}
     rS = roman_string
+
     if rS is None or type(rS) is not str:
         return None
 	output = 0
 	prev = 1001
-	for val in rS:
-		curr = int(rome.get(val))
-		if curr <= prev:
-			output += curr
-		else:
-			if output > curr:
-				output -= curr
-			else:
-				output = (output - curr) * -1
-		prev = curr
-	return output
+
+    for val in rS:
+        curr = int(rome.get(val))
+        if curr <= prev:
+            output += curr
+        else:
+            if output > curr:
+                output -= curr
+            else:
+                output = (output - curr) * -1
+                prev = curr
+    return output
