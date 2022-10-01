601a4952c242b305617526ba71d1043f9bef3711
michaelEmeka <mikelonu15@gmail.com>
2022-10-01 16:31:33 +0100

    Modify file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rome = {'M' : 1000, 'D': 500, 'C': 100, 'L': 50, 'X':10, 'V': 5, 'I':1}
+    rome = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     rS = roman_string
 
     if rS is None or type(rS) is not str:
