e2c6b2372b4f87b738ab32a61dad65b5b9b1889b
michaelEmeka <mikelonu15@gmail.com>
2022-10-01 20:29:28 +0100

    Add 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    p = 1
+    addition = 0
+    divisor = 0
+    for i in range(len(my_list)):
+        for j in range(len(my_list[i])):
+            p *= my_list[i][j]
+        divisor += my_list[i][j]
+        addition += p
+        p = 1
+    return addition / divisor
