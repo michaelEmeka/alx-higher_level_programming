21ea9913cc61a24f442ef785ab3bf0a23910b5e5
michaelEmeka <mikelonu15@gmail.com>
2022-10-12 17:18:14 +0100

    Modify 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define a class, Square"""
+
+
 class Square:
-    """An empty Square class that defines a square"""
+    """An empty Square class that defines a square."""
     pass
