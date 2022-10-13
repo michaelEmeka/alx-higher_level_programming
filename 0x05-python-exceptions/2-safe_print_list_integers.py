4835e1481a2688af05fd78b4c4c1b7fd7004aa9d
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 14:48:58 +0100

    Modify file 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -19,7 +19,6 @@ def safe_print_list_integers(my_list=[], x=0):
             p += 1
         except(TypeError, ValueError):
             continue
-        except IndexError:
-            break
+
     print()
     return p
