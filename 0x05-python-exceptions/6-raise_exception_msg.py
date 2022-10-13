ca00e53513a4d08128089d31ca7597e7026d97be
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 16:38:39 +0100

    Add 6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """raises a name exception with a message
+    Args:
+        message: name exception message
+    """
+    raise NameError(message)
