70994a88bf0de1467384c563fe4c5dc13024994a
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 13:36:05 +0100

    Modifyb0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[n], end="")
             p = n + 1
-        except:
+        except IndexError:
             break
     print()
     return p
