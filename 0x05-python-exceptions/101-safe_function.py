3b2ccf9833ad866565d5a0c009d5991e4df7befc
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 18:10:55 +0100

    Modify file 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -13,7 +13,7 @@ def safe_function(fct, *args):
     b = args[1]
     try:
         ret = fct(a, b);
-    except ZeroDivisionError as err:
+    except Exception as err:
         stderr.write("Exception: {}\n".format(err))
         ret = None
 
