15b7a2031ff6471931a724f24f5386adfbe2a53e
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 15:39:06 +0100

    Add 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    This function divides my_list_1 by my_list_2 based
+    on list_length to be divided
+
+    Args:
+        my_list_1: dividend list
+        my_list_2: divisor list
+        list_length: list length to be divided
+    """
+    new_list = my_list_1.copy()
+    for i in range(list_length):
+        try:
+            val = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            val = 0
+            continue
+        except (TypeError, ValueError):
+            print("wrong type")
+            val = 0
+            continue
+        except IndexError:
+            print("out of range")
+            val = 0
+            break
+        finally:
+            new_list[i] = val
+
+    return new_list
