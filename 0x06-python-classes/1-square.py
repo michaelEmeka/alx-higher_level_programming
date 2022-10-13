943fe1dc0bd6b1544d1e9854645964efa4e2ae99
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 11:28:45 +0100

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+class Square:
+    """Defines a new square based on:
+    args:
+        size
+        """
+    def __init__(self, size):
+        self.size = size
