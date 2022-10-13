73049d0681c402933510f693eb7e310a098b5356
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 18:01:17 +0100

    Add file 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import stderr:
+
+def safe_function(fct, *args):
+    """
+    This function calls a function fct.
+
+    Args:
+        fct: a pointer to a function
+        args: arguments for fct
+    """
+    a = int(args[0])
+    b = int(args[1])
+    try:
+        ret = fct(a, b);
+    except ZeroDivisionError as err:
+        stderr.write("Exception: {}\n".format(err))
+        ret = None
+
+    return ret
