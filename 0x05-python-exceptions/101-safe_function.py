673e57d248c33efaf6fa8990c8f9978973af5fb8
michaelEmeka <mikelonu15@gmail.com>
2022-10-13 18:14:18 +0100

    Modify file 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import stderr
 
+
 def safe_function(fct, *args):
     """
     This function calls a function fct.
@@ -9,10 +10,8 @@ def safe_function(fct, *args):
         fct: a pointer to a function
         args: arguments for fct
     """
-    a = args[0]
-    b = args[1]
     try:
-        ret = fct(a, b);
+        ret = fct(*args)
     except Exception as err:
         stderr.write("Exception: {}\n".format(err))
         ret = None
