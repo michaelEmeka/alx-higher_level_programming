8baba354fce55bc2b8f48a655a515a3e82f70d14
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 16:24:06 +0100

    Modify file 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Defines a square
-"""
+"""Defines a class Square"""
 
 
 class Square:
@@ -44,4 +42,3 @@ class Square:
         Returns: Area of square.
         """
         return self.__size ** 2
-
