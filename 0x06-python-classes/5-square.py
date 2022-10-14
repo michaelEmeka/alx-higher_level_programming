0beb50f7306ff67f94c452905ff1a16e0315854a
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 16:41:25 +0100

    Add 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """A Square class."""
+    def __init__(self, size=0):
+        """
+        Instantiates a square class.
+        Args:
+            size: A square's size.
+        """
+        self.__size = size
+
+    @getter
+    def size(self):
+        """
+        Retrieve square's size private instance attribute.
+        Returns: size.
+        """
+        return self.__size
+
+    @setter
+    def size(self, value):
+        """
+        Sets a square's size private attribute.
+        Args:
+            value: square's size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = num
+
+    @property
+    def area(self):
+        """
+        Returns: Area of square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints a square.
+        """
+        lb = self.__size
+        if lb == 0:
+            print()
+        else:
+            for i in range(lb):
+                for j in range(lb):
+                    print("#")
