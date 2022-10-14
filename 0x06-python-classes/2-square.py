d744648bdb080e63490c55e726dff189d4a83102
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 14:51:03 +0100

    Modify file 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     """
     Defines a square.
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Initializes a square instance.
         Args:
