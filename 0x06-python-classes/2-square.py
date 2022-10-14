d0d2102548ebdda0bad56c424d8c2d64a4eea226
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 14:38:21 +0100

    Add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+class Square:
+    """
+    Defines a square
+    """
+    def __init__(self, size):
+        """
+        Initializes a square instance.
+        Args:
+            size: square private size instamce attribute.
+            self: class instance
+        """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        try:
+            num = size / 1
+        except TypeError:
+            raise TypeError("size must be an integer")
+        self.__size = num
