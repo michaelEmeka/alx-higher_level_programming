1f5679907442ed249e9e034bed922586987b377f
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 20:21:50 +0100

    Mistakenly delete all files

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,4 +7,4 @@ def no_c(my_string):
         if my_string[i] in delim:
             my_string.remove(my_string[i])
     my_string = "".join(my_string)
-    return my_string
+    return my_strin
--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Define a class, Square"""
-
-
-class Square:
-    """An empty Square class that defines a square."""
-    pass
