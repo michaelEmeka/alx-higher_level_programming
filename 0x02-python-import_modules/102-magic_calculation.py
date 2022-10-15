5323f8e8d82e9ae3f9e72164c5232d162753839c
michaelEmeka <mikelonu15@gmail.com>
2022-10-15 03:03:19 +0100

    Add 102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """
+    Performs a calculation likewise a given python bytecode.
+    """
+    add  = __import__("magic_calculation_102").add
+    sub = __import__("magic_calculation_102").sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+    return None
