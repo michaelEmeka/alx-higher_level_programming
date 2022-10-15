96c0c1ca349a303f95674d36bba7384551985b59
michaelEmeka <mikelonu15@gmail.com>
2022-10-15 02:26:16 +0100

    Add 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    dis.dis(magic_calculation)
+    """
+    A reverse interpretation of a python bytecode.
+    """
+    if a < b:
+        return c
+    elif c < b:
+        return (a + b)
+    else:
+        return (a * b) - c
