9c0936f5cd10f3408cad8982344a883ad5e74cbb
michaelEmeka <mikelonu15@gmail.com>
2022-10-27 15:19:23 +0100

    Add file 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Defones a class MyList."""
+
+
+class MyList(list):
+    """
+    A class MyList that inherits from list.
+    Args:
+        list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints sorted list.
+        """
+        p = self.copy()
+        p.sort()
+        print(p)
