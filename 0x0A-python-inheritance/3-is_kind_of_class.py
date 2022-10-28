1baeefa00ac019f3f892817475a13e001ee61925
michaelEmeka <mikelonu15@gmail.com>
2022-10-28 08:55:36 +0100

    Modify file 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that checks for kind of class."""
 
+
 def is_kind_of_class(obj, a_class):
     """
     Checks if obj is an instance of a_class or another class
