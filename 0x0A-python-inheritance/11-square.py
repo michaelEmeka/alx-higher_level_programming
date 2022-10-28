6f571b4d21380ec30b4adcccf6681291cdfa860e
michaelEmeka <mikelonu15@gmail.com>
2022-10-28 15:44:32 +0100

    Add file 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,16 +5,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    A square class inherited from a rectangle class.
+    A square class.
     """
     def __init__(self, size):
         """
         Instantiates a square.
         Args:
-            size: square size(a private instance attribute)
+            size: private instance attribute(square size).
         """
         self.integer_validator("size", size)
         self.__size = size
-        self.__width = size
         self.__height = size
+        self.__width = size
         super().__init__(size, size)
