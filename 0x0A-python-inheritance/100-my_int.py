894be350e6cf23b11758c9543ec48bcb0a0b8a10
michaelEmeka <mikelonu15@gmail.com>
2022-10-28 16:35:29 +0100

    Add 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a rebel class."""
+
+
+class MyInt(int):
+    """
+    A rebel class(has == and != operators inverted).
+    """
+    def __eq__(self, value):
+        """
+        Overrides == operator with != behavior.
+        Args:
+            value: int instance.
+        """
+        return self.real != value
+    
+    def __ne__(self, value):
+        """
+        Overrides != operator with == behavior.
+        Args:
+            value: int instance.
+        """
+        return self.real == value
