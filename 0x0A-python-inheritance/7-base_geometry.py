a09afdcb48c110ff3bf7fc58e11eee83ec241d0c
michaelEmeka <mikelonu15@gmail.com>
2022-10-28 13:28:18 +0100

    Add 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    A class.
+    """
+    def area(self):
+        """Raises an exception."""
+        raise Exception("area() is not implemented");
+
+    def integer_validator(self, name, value):
+        """
+        This function validates value.
+        Args:
+            name: integer variable name.
+            value: an integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        pass
