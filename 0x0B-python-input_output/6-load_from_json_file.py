d628c9da011f63e7e26a765bf90aeb0b407ed36c
michaelEmeka <mikelonu15@gmail.com>
2022-10-29 17:18:18 +0100

    Modify 7-add_item.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def load_from_json_file(filename):
     """Loads a json_file and creates an object from it."""
-    with open(filename, "w+", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         x = json.loads(read_data)
         return (x)
