c1a1a2356faa9b7ab639eea04eb8b441cc35093f
michaelEmeka <mikelonu15@gmail.com>
2022-10-30 19:59:37 +0100

    Add 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,21 +3,21 @@
 
 
 class Student:
-    """
-    Defines a Student.
-    """
+    """A Student calss."""
     def __init__(self, first_name, last_name, age):
-        """Instantiates a student."""
+        """
+        Instantiates a Student instance.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a student
+        Retrieves a dictionary representation of a Student
         instance.
         Args:
-            attrs: attrs to be included in dictionary(optional).
+            attrs: attributes needed in our dictionary.(optional).
         """
         if attrs is None:
             return self.__dict__
@@ -25,3 +25,12 @@ class Student:
                 all(type(attr) == str for attr in attrs)):
             return {k: getattr(self, k) for k in attrs
                     if hasattr(self, k)}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance.
+        Args:
+            json: json dictionary(deserialized).
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
