ac8cc75109a3abcac571582be5afec6c9ab23fb3
michaelEmeka <mikelonu15@gmail.com>
2022-11-01 16:57:12 +0100

    Add models and tests folders.

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class named base."""
+
+
+class Base:
+    """Base class."""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Instantiates a class.
+        Args:
+            id: instance id.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
