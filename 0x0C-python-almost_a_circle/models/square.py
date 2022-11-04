6140750869e576f72e1c8982b5e9f65c6a84db47
michaelEmeka <mikelonu15@gmail.com>
2022-11-04 22:22:46 +0100

    Modify models/square.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Instantiates a square."""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Set/get square size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.width = self.height = value
