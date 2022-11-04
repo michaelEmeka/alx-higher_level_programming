dfad7159556e96f9b1e06f0cb3d054ebdb977285
michaelEmeka <mikelonu15@gmail.com>
2022-11-04 18:19:47 +0100

    Modify models/rectangle.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """Returns the area of triangle."""
+        return (self.__width * self.__height)
