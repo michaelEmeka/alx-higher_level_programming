bee387e3cd106df91461d718ac2c0f1a05e134ce
michaelEmeka <mikelonu15@gmail.com>
2022-11-21 18:29:06 +0100

    Modify file 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tupple(tuple_a=(), tuple_b=()):
+def add_tuple(tuple_a=(), tuple_b=()):
     sum_tuple = ()
     tuple_1 = tuple_a + (0, 0)
     tuple_2 = tuple_b + (0, 0)
