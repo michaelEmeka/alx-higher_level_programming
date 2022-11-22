1db28914c228bc9371c3907f2eba3c2bf706d3cd
michaelEmeka <mikelonu15@gmail.com>
2022-11-22 14:56:41 +0100

    Modify file 6.

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     Defines a rectangle.
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Instantiates a rectangle imstance.
