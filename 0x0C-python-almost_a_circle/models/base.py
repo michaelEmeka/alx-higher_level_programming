dc530801d872edae828632f6c203c5507cf37332
michaelEmeka <mikelonu15@gmail.com>
2022-11-22 19:35:24 +0100

    Modify models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,11 +19,22 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Returns the JSON string representation of
-        list_dictionaries.
+        list_dictionaries if it is not None.
+        else: "[]" string.
         """
         if list_dictionaries is not None:
             return (json.dumps(list_dictionaries))
         return "[]"
+
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs
+        to a file.
+        """
+        filename = list_objs[0].__class__.__name__ + ".json"
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(to_json_string)
