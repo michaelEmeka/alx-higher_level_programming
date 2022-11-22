0ec4a9bbade1e4901462ec8a33c938d24868c802
michaelEmeka <mikelonu15@gmail.com>
2022-11-22 18:32:14 +0100

    Modify models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a class named base."""
+import json
 
 
 class Base:
@@ -17,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of
+        list_dictionaries.
+        """
+        return (json.dumps(list_dictionaries))
